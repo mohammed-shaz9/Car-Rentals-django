eaa9525a989433b0f1d084f7e3f93a0fb46d2c6b
mohammed-shaz9 <mohammedsnz2000@gmail.com>
2026-02-06 23:23:47 +0530

    Use robust static files storage to prevent startup crash

--- vehicles/settings.py
+++ vehicles/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # managing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
