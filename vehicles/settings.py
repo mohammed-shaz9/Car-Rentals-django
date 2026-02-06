518001713ba6024c2233487ab3e6df8c0e700560
mohammed-shaz9 <mohammedsnz2000@gmail.com>
2026-02-06 23:22:08 +0530

    Silence DB warning: only configure postgres if DATABASE_URL is set

--- vehicles/settings.py
+++ vehicles/settings.py
@@ -95,8 +95,10 @@ DATABASES = {
 }
 
 # Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# Update database configuration from $DATABASE_URL.
+if os.environ.get('DATABASE_URL'):
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
